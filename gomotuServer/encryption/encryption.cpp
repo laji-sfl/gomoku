7b5c972931ee47d1d941171c26b5e8bb59fe8587
laji-sfl <sfl_wyyx@163.com>
2018-07-05 16:41:20 +0800

    start coding!

--- gomotuServer/encryption/encryption.cpp
+++ gomotuServer/encryption/encryption.cpp
@@ -1,10 +1,22 @@
 /*
  *	实现客户端与服务器之间的通信数据的简单加密
  *  利用openssl库中的加密模块来实现
+ *
+ *  学习加密库的难点：
+ *      1、对于加密的原理不清楚，所以难以理解库函数的schedule，向量，等名称表示的是什么意思，不
+ *  理解加密的字节长度要求的原因，造成学习库函数调用的时候，不知该调用那个函数，调用顺序是什么？
+ *      2、英文水平不过关，阅读官方文档有障碍。
+ *      3、查询Google发现没有完整的论述，只是写出了某种加密方法调用，难以理解（没有写出我不懂的）
+ *  总结：
+ *      1、从整体上了解对称加密和非对称加密，还有摘要的区别与实现
+ *      2、了解到对称加密的不同方法还有不同模式的区别，但是整体的加密方法是不变的
+ *      3、需要将以上问题及其答案总结出来，方便以后复习（加密算法的分类、原理、常用算法的应用）
  */
 #include <stdio.h>
 #include <string.h>
 #include <openssl/des.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
 #include <openssl/pkcs7.h>
 #include <openssl/md5.h>
 #include <openssl/aes.h>
@@ -58,8 +70,67 @@ void md5_test()
 
 /*DES对称秘钥加密*/
 /*
+ *  加密是块加密，需要将每次加密的文本长度限制在8字节（不同模式可能不同）。
  *  函数接口介绍：
+ *  DES_random_key(),首先调用生成随机的key schedule
+ *  DES_set_key_checked(),设置key，并检查奇偶校验位
+ *  DES_ecb_encrypt(),加密，解密靠参数决定，ecb模式输入输出必须是8字节，需要自己分，
+ *      密码也是8字节
+ *  DES_ncbc_encrypt(),输入可以不是8字节的倍数，但是输出一定是，函数内部会填充，密码是8字节
+ *  DES_set_key_unchecked,不检查奇偶校验
+ *  DES_string_to_key,根据string生成一个随机的秘钥，秘钥8字节
  */
+void des_encrypt_test(/*ecb模式，没有设置密码，没有对长字符串进行分解*/)
+{
+    //初始变量
+    DES_cblock key;
+    DES_key_schedule schedule;
+
+    //设置密码key
+    DES_random_key(&key);
+    DES_set_key_checked(&key, &schedule);
+
+    DES_cblock input = "hello", output; //类型为8字节的字节数组
+
+    //加解密
+    DES_ecb_encrypt(&input, &output, &schedule, DES_ENCRYPT);
+    printf("jia mi hou: %s\n", output);
+    DES_ecb_encrypt(&output, &input, &schedule, DES_DECRYPT);
+    printf("jie mi hou: %s\n", input);
+}
+
+//cbc模式，自己设置密码，对长于8字节的原文本加解密
+void des_encrypt_test2(unsigned char *plaintext,
+                       unsigned char *ciphetext,
+                       char *key)
+{
+    //初始化变量
+    DES_cblock desKey;
+    DES_cblock ivec;    //向量
+    DES_key_schedule schedule;
+
+    //根据密码来生成一个可以用来加密的key
+    DES_string_to_key(key, &desKey);//密码长度为8字节（最多，自己的那个密码字符串可以更长）
+    DES_set_key_unchecked(&desKey, &schedule);
+
+    //设置向量值
+    memset(&ivec, 0, 8);
+
+    //test
+    unsigned char in[16] = "helloooasdq";
+    unsigned char out[16] = {0};
+    unsigned char jiemihou[16] = {0};
+
+    //加密
+    DES_ncbc_encrypt(in, out, 16, &schedule, &ivec, DES_ENCRYPT);
+    printf("jiamihou: %s\n", out);
+
+    //解密
+    memset(&ivec, 0, 8);        //当将向量置为最初的值时，可以解密出明文？？
+    //帮助文档说ncbc会改变向量，而cbc不会（cbc是bug）
+    DES_ncbc_encrypt(out, jiemihou, 16, &schedule, &ivec, DES_DECRYPT);
+    printf("jiemihou: %s\n", jiemihou);
+}
 
 /*AES对称加密加密*/
 /*
@@ -119,6 +190,128 @@ void aes_decrypt_test(char *plaintext, char *ciphetext, char *key)
     }
 }
 
+/*RSA非对称加密*/
+/*
+ *  生成一个公钥和私钥，公钥加密私钥解密，私钥加密公钥解密（就是签名）
+ *  函数接口：
+ *      RSA_generate_key(),产生一个秘钥对
+ *      不了解BIO是什么和BIO操作的作用和方法
+ */
+//创建一个秘钥对,写入本地文件
+void generateRSAkey(char *pubOut, char *priOut)
+{
+#define KEY_LEN 2048    //秘钥长度
+
+    //产生一个秘钥对,不了解第2、3、4个参数
+    RSA *keypair = RSA_generate_key(KEY_LEN, RSA_3, NULL, NULL);
+//    RSA_generate_key_ex( (??如何定义变量), 2048, RSA_3, NULL);
+
+    //不懂，这时什么接口函数
+    BIO *pri = BIO_new(BIO_s_mem());
+    BIO *pub = BIO_new(BIO_s_mem());
+
+    //不懂函数原理
+    PEM_write_bio_RSAPrivateKey(pri, keypair, NULL, NULL, 0, NULL, NULL);
+    PEM_write_bio_RSAPublicKey(pub, keypair);
+
+    //获取长度
+    size_t pri_len = BIO_pending(pri);
+    size_t pub_len = BIO_pending(pub);
+
+    //分配秘钥字符串内存
+    char *pri_str = (char *)malloc(pri_len + 1);
+    char *pub_str = (char *)malloc(pub_len + 1);
+
+    BIO_read(pri, pri_str, pri_len);
+    BIO_read(pub, pub_str, pub_len);
+
+    pri_str[pri_len] = '\0';
+    pub_str[pub_len] = '\0';
+
+    //传出参数，输出
+    printf("pub_key len: %d\n", pub_len);
+    printf("pri_key len: %d\n", pri_len);
+    memcpy(pubOut, pub_str, pub_len+1);
+    memcpy(priOut, pri_str, pri_len+1);
+
+    //存储到磁盘（这种方式存储的是begin rsa public key/ begin rsa private key开头的）
+    //fputs遇到\0就会结束，可能造成写入文件不完全（是否会存在\0）
+    FILE *pubFile = fopen("./pub_key_str", "w");
+    if (pubFile == NULL) {
+        return;
+    }
+    fputs(pub_str, pubFile);
+    fclose(pubFile);
+
+    FILE *priFile = fopen("./pri_key_str", "w");
+    if (priFile == NULL) {
+        return;
+    }
+    fputs(pri_str, priFile);
+    fclose(priFile);
+
+    //释放资源
+    RSA_free(keypair);
+    BIO_free_all(pub);
+    BIO_free_all(pri);
+
+    free(pri_str);
+    free(pub_str);
+}
+
+//RSA公钥加密
+void rsa_pub_encrypt(char *plaintext, char *pubstr, char *ciphetext)
+{
+    //秘钥对,和秘钥字符串不同，需要在内存中根据字符串计算秘钥对
+//    RSA *rsa = NULL;
+    BIO *keybio = BIO_new_mem_buf((unsigned char *)pubstr, -1);
+    RSA *rsa = RSA_new();
+    rsa = PEM_read_bio_RSAPublicKey(keybio, &rsa, NULL, NULL);
+
+    int len = RSA_size(rsa);
+    printf("rsa_size len: %d\n", len);
+    char *beEncrypt = (char *)malloc(len + 1);
+    memset(beEncrypt, 0, len + 1);
+
+    //加密
+    int ret = RSA_public_encrypt(strlen(plaintext), (unsigned char *)plaintext,
+                       (unsigned char *)beEncrypt, rsa, RSA_PKCS1_PADDING);
+    printf("ret: %d\n", ret);
+    printf("before: %s\n", plaintext);
+    printf("after: %s\n", beEncrypt);
+    memcpy(ciphetext, beEncrypt, ret);
+
+    free(beEncrypt);
+    BIO_free_all(keybio);
+    RSA_free(rsa);
+}
+
+//私钥解密
+void rsa_pri_decrypt(char *ciphetext, char *pristr)
+{
+    RSA *rsa = RSA_new();
+    BIO *keybio;
+    keybio = BIO_new_mem_buf((unsigned char *)pristr, -1);
+
+    rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
+
+    int len = RSA_size(rsa);
+    char *beDecrypt = (char *)malloc(len + 1);
+    memset(beDecrypt, 0, len + 1);
+
+    // 解密函数
+    int ret = RSA_private_decrypt(strlen(ciphetext), (unsigned char*)ciphetext,
+                                  (unsigned char*)beDecrypt, rsa,
+                                  RSA_PKCS1_PADDING);
+    printf("ret pri: %d\n", ret);
+    printf("decrypt: %s\n", beDecrypt);
+
+    // 释放内存
+    free(beDecrypt);
+    BIO_free_all(keybio);
+    RSA_free(rsa);
+}
+
 int main()
 {
 //   md5_test();
@@ -134,6 +327,23 @@ int main()
 //    memset(in, 0, 160);
 //    aes_decrypt_test(in, out, key);
 //    printf("jie mi hou: %s\n", in);
+
+//    des_encrypt_test();
+
+//    unsigned char in[18] = "hello you really!";
+//    unsigned char out[18] = {0};
+//    char mima[8] = "qwertwe";
+//    des_encrypt_test2(in, out, mima);
+
+//    char pubstr[2049] = {0}, pristr[2049] = {0}, ciphetext[1024] = {0};
+//    generateRSAkey(pubstr, pristr);
+//    printf("pub: %s\n", pubstr);
+//    printf("pri: %s\n", pristr);
+
+//    char text[] = "hello i am your father!!";
+
+//    rsa_pub_encrypt(text, pubstr, ciphetext);
+//    rsa_pri_decrypt(ciphetext, pristr);
 
     return 0;
 }
